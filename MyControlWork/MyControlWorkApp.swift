809619c25a1c82d1117810bdc8947075a2a3f593
Leha228 <ryabin.01@mail.ru>
2022-10-18 21:22:00 +0500

    Initial Commit

---
+++ MyControlWork/MyControlWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyControlWorkApp.swift
+//  MyControlWork
+//
+//  Created by Александр Шандыба on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyControlWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
